26b8d18c2bcb2159980f5f8a927cf481102c93a6
Felipe Alves de Oliveira <felpsVoid@proton.me>
2025-12-04 09:46:38 -0300

    fazendo alterações

--- projetos/caixaV2.py
+++ projetos/caixaV2.py
@@ -1,40 +1,9 @@
 from datetime import date
+import funcoes as fc
 
 iniciando = True
 programa_ativo = True
-precoTotal = 0
 carrinho = {}
-
-def visualizar(dic):
-  global precoTotal
-  for produto, dados in dic.items():
-    print(f'Produto: {produto} | Preço: {dados['Preco']} | Quantidade: {dados['Quantidade']}')
-    precoTotal += dados['Preco']*dados['Quantidade']
-    
-  if precoTotal >= 100:
-    precoTotal *= 0.90 # 10% desconto
-  elif precoTotal > 50:
-    precoTotal *= 0.95 # 5% desconto
-  else:
-    pass
-
-def pesquisa(item_pesquisado,dic):
-  parte = item_pesquisado.lower()
-  encontrados = []
-
-  for nome , dados in dic.items():
-    if parte in nome.lower():
-      encontrados.append((nome,dados))
-  
-  if encontrados:
-    print('\n------ Resultados encontrados ------')
-    for nome, dados in encontrados:
-      print(f'Produto: {nome} | Preço: {dados['Preco']} | Quantidade: {dados['Quantidade']}')
-  else:
-    print(f'Nenhum produto com o nome: {item_pesquisado}, foi encontrado!')
-
-def ediacao(item_editar,dic):
-  pass
 
 # inicio do programa
 while iniciando:
@@ -43,26 +12,26 @@ while iniciando:
     preco = float(input("Preço: "))
     qtd = int(input("Qtd: "))
 
-    carrinho[prod] = {'Preco': preco, 'Quantidade': qtd}
+    carrinho[prod] = {'preco': preco, 'quantidade': qtd}
 
     print('\n----------- Produto adicionado -----------')
     print(f'Produto: {prod} | Preço: {preco} | Quatidade: {qtd}')
     print('------------------------------------------\n')
     
-    cond = int(input('1-Pesquisar um item\n2-Alterar um item\n3-Finalizar comprar\n4-continuar a compra\nqual das opções deseja: '))
+    cond = int(input('1-Pesquisar um item\n2-Alterar um item\n3-Finalizar comprar\n4-continuar a compra\n\nqual das opções deseja: '))
     opt = True
     while opt:
       if cond != '':
         if cond == 1:
           nomeProduto = str(input('\nQual o nome do produto: '))
-          pesquisa(nomeProduto,carrinho)
+          fc.pesquisa(nomeProduto,carrinho)
           break
         elif cond == 2:
-         pass
+          nomeProduto = str(input('\nDigite o nome do produto: '))
+          fc.ediacao(nomeProduto, carrinho)
+          break
         elif cond == 3:
-          print(f'\n-------------- Data: {date.today().strftime('%d/%m/%Y')} --------------\n')
-          visualizar(carrinho)
-          print(f'\nTotal: {precoTotal:.2f}')
+          fc.visualizar(carrinho)
           opt = False
           iniciando = False
         elif cond == 4:
