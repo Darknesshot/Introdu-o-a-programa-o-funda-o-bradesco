654f9d8d87b3d9b9cef5cbdc99704e4d93630bc1
Felipe Alves <felipe.oli468@gmail.com>
2025-12-09 18:18:09 -0300

    Adicionando a função de transferencia

--- projetos/banco/main.py
+++ projetos/banco/main.py
@@ -7,10 +7,24 @@ programa_ativo = True
 iniciando = True
 
 def depositar(valorInformado):
+  global saldo
   valor = valorInformado
+  saldo += valor
   
+def transferencia(valorInformado, numeroDaConta):
+  global saldo
+  valor = valorInformado
+  nConta = numeroDaConta
+
+  if valor > 0 and nConta != 0:
+    saldo -= valor
+    print(f'Valor foi transferido para conta: {nConta}')
+    print(f'Valor: {valor}')
+  else:
+    print('valor ou o número da conta estão incorretos!')
 
 def saque(valorInformado, senhaInformada):
+  global saldo
   valor = valorInformado
   senhaAutentificacao = senhaInformada
   if saldo <= 0 and senhaAutentificacao == senha:
@@ -26,12 +40,23 @@ def gerenciamento():
     case 1:
       print("------------ Dados da conta ------------\n")
       print(f"Nome do usuário: {nome}\nSaldo em conta: {saldo}")
+    case 2:
+      numeroDaConta = int(input('Informe o numero da conta: '))
+      valor = float(input('Valor da transferencia: '))
+      transferencia(valor,numeroDaConta)
     case 3:
       valor = float(input("Qual o valor que deseja depositar: "))
+      depositar(valor)
     case 4:
       valor = float(input("Qual valor deseja retirar: "))
       senhaAuten = int(input('Informe a senha: '))
       saque(valor, senhaAuten)
+    case 5:
+      print('Encerrando!')
+      global iniciando
+      global programa_ativo
+      iniciando = False
+      programa_ativo = False
     case _:
       print('Desculpe mas não exite essa opção')
 
@@ -39,10 +64,12 @@ def login(nomeDigitado, senhaDigitada):
   if nomeDigitado == nome and senhaDigitada == senha:
     print('\nUsuário confirmado!')
     print('Acessando conta!!!')
+    gerenciamento()
 
   else:
     print(f'O usuário: {nomeDigitado} ou senha: {senhaDigitada} estão incorretas')
 
+# Programa
 while iniciando:
   if programa_ativo:
     nomeInformado = str(input("Informe seu nome: "))
